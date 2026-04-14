644c7af1b70945b8d9d33a6aaed7d6cb41e9ae35
Seemanshu Verma <seemanshu0409@gmail.com>
2026-04-14 23:22:43 +0530

    main merge line

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("I am running feature1.")
 print("Feature 1 added.")
 
-print("I am adding, and changing  a merge conflict line ")
+print("I am adding, and changing , and fixing a merge conflict line ")
 
 print("I am running Main.Added Merge conflict .")
 print("Main should be added main.")
